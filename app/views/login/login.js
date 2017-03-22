72b74075b66fbc1a93043fe24257036453fa2a35
Robert Hafner <tedivm@tedivm.com>
2017-03-21 20:14:52 -0700

    iOS graphics

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -3,16 +3,24 @@ var Session =    require('../../services/session.js')
 var frames = require("ui/frame");
 var page;
 
+var isAttempting = false
+
 exports.loaded = function(args) {
   page = args.object;
   var testing = {}
   page.bindingContext = testing
   page.bindingContext['activity'] = false
+  isAttempting = false
   //testing["activity"] = false
   //page.bindingContext.set("activity",false)
 };
 
 exports.signIn = function() {
+
+  if(isAttempting) {
+    return
+  }
+  isAttempting = true
 
   var username_view = page.getViewById("username");
   var password_view = page.getViewById("password");
@@ -21,6 +29,14 @@ exports.signIn = function() {
   var username = username_view.text;
   var password = password_view.text;
 
+  if(!!ScreepsAPI.token) {
+    if(ScreepsAPI.username == username) {
+      if(ScreepsAPI.password == password) {
+        // already logged in as this user- probably hit the button too fast.
+        return
+      }
+    }
+  }
 
 
   ScreepsAPI.username = username
@@ -43,8 +59,10 @@ exports.signIn = function() {
       alert('sign in failed')
       password_view.text = ''
     }
+    isAttempting = false
   })
   .catch(function (response){
+    isAttempting = false
     console.log(response.message)
     page.bindingContext['activity'] = true
     alert('sign in failed')
